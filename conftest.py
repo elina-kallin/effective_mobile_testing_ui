7a5579d13eb26e815534fa558224064a025c664f
Елена Бакальская <elina_kallin@mail.ru>
2026-02-19 14:41:39 +0400

    поправила название переменной окружения SELENOID в conftest

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.firefox.options import Options
 
 @pytest.fixture(scope="module")
 def browser():
-    remote_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+    remote_url = os.getenv("SELENOID_REMOTE_URL", "http://localhost:4444/wd/hub")
     options = Options()
     options.add_argument("--headless")
     driver = webdriver.Remote(command_executor=remote_url, options=options)
