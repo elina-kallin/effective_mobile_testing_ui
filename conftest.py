56914d385eae696e62929ecac4964a0c6191a6f4
Елена Бакальская <elina_kallin@mail.ru>
2026-02-20 14:21:46 +0400

    проверка запуска CI по коммиту (пушу) в репозиторий

--- conftest.py
+++ conftest.py
@@ -8,8 +8,16 @@ from selenium.webdriver.firefox.options import Options
 def browser():
     remote_url = os.getenv("SELENOID_REMOTE_URL", "http://localhost:4444/wd/hub")
     options = Options()
+
+    selenoid_options = {
+        "enableVNC": True, 
+        "enableVideo": False,
+        "screenResolution": "1280x1024x24"  
+    }
+
     options.set_capability("browserName", "firefox")
-    options.add_argument("--headless")
+    options.set_capability("selenoid:options", selenoid_options)
+
     driver = webdriver.Remote(command_executor=remote_url, options=options)
     yield driver
     driver.quit()
